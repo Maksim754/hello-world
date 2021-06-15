882dbe9b95c0875ca47ae19eee71d385169dea96
Maksim754 <galimov.maksim3@gmail.com>
2021-06-15 08:26:28 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " +  world + "!");
+
+
+//        int a = 1;
+//        int b = 2;
+//        System.out.println("a < b = " + (a < b));
+//        System.out.println("a > b = " + (a > b));
+
+
+//
+//        int a = 1;
+//        int b = 2;
+//        int c = a +b;
+//        System.out.println("sum = " + c);
+
+
+//        int i = 1;
+//        System.out.println("i");
+//        i = i + 1;
+//        System.out.println("i");
+//        i = i + 1;
+//        System.out.println("i");
+//        System.out.println("Hello World!");
+    }
+}
